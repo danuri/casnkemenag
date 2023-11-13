dcb858fa1de0d20d270e03f6c665a5d4edaf6ebb
danuri <danualbantani@gmail.com>
2023-11-13 19:00:32 +0700

    Update pg.php

--- app/Views/pg.php
+++ app/Views/pg.php
@@ -54,6 +54,50 @@
                 </div>
               </div>
             </div>
+
+            <div class="card">
+              <div class="card-header">
+                PPPK FUNGSIONAL
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table align-middle table-striped-columns mb-0">
+                    <thead>
+                      <tr>
+                        <th>Materi</th>
+                        <th>Jumlah Soal</th>
+                        <th>Nilai Maksimal</th>
+                        <th>PG</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Seleksi Kompetensi Teknis</td>
+                        <td>90</td>
+                        <td>450</td>
+                        <td>Berdasarkan Jenis Jabatan</td>
+                      </tr>
+                      <tr>
+                        <td>Seleksi Kompetensi Manajerial</td>
+                        <td>25</td>
+                        <td colspan="2">180</td>
+                        <td colspan="2">117</td>
+                      </tr>
+                      <tr>
+                        <td>Seleksi Kompetensi Sosio Kultural</td>
+                        <td>20</td>
+                      </tr>
+                      <tr>
+                        <td>Wawancara</td>
+                        <td>10</td>
+                        <td>40</td>
+                        <td>24</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
