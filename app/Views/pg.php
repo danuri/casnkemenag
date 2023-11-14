714e7becaea20363e8f20778a4cc8a26b68d5579
danuri <danualbantani@gmail.com>
2023-11-14 08:44:44 +0700

    Update pg.php

--- app/Views/pg.php
+++ app/Views/pg.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">PENGUMUMAN</h4>
+                    <h4 class="mb-sm-0">PASSING GRADE</h4>
                 </div>
             </div>
         </div>
