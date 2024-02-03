600c4e281cce37e11cf64c06c1ba51506d1835d3
danuri <danualbantani@gmail.com>
2024-02-03 12:13:19 +0700

    Update pengumuman.php

--- app/Views/pengumuman.php
+++ app/Views/pengumuman.php
@@ -62,6 +62,36 @@
                         <td>Pengumuman Pemilihan Titik Lokasi Ujian Seleksi Kompetensi Teknis Tambahan (SKTT) Calon PPPK Kemenag 2023</td>
                         <td><a href="https://cdn.kemenag.go.id/storage/archives/pengumuman-pemilihan-titik-lokasi-skttpdf.pdf" target="_blank">Download</a></td>
                       </tr>
+                      <tr>
+                        <td>09-12-2023</td>
+                        <td>Pengumuman Pelaksanaan Seleksi Kompetensi Teknis Tambahan (SKTT) Calon PPPK Kemenag Tahun 2023</td>
+                        <td><a href="https://kemenag.go.id/informasi/pengumuman-pelaksanaan-seleksi-kompetensi-teknis-tambahan--sktt--calon-pppk-kemenag-tahun-2023" target="_blank">Download</a></td>
+                      </tr>
+                      <tr>
+                        <td>09-12-2023</td>
+                        <td>Penyesuaian Lokasi dan Jadwal Seleksi Kompetensi Teknis Tambahan (SKTT) Calon PPPK Kemenag Tahun 2023</td>
+                        <td><a href="https://kemenag.go.id/informasi/penyesuaian-lokasi-dan-jadwal-seleksi-kompetensi-teknis-tambahan--sktt---calon-pppk-kemenag-tahun-2023" target="_blank">Download</a></td>
+                      </tr>
+                      <tr>
+                        <td>13-12-2023</td>
+                        <td>Pelaksanaan Seleksi Kompetensi Bidang CPNS Kemenag Tahun 2023</td>
+                        <td><a href="https://kemenag.go.id/informasi/pelaksanaan-seleksi-kompetensi-bidang-cpns-kemenag-tahun-2023" target="_blank">Download</a></td>
+                      </tr>
+                      <tr>
+                        <td>13-12-2023</td>
+                        <td>Daftar Nama Peserta Pelaksanaan Seleksi Kompetensi Bidang CPNS Kemenag Tahun 2023</td>
+                        <td><a href="https://kemenag.go.id/informasi/daftar-nama-peserta-pelaksanaan-seleksi-kompetensi-bidang-cpns-kemenag-tahun-2023" target="_blank">Download</a></td>
+                      </tr>
+                      <tr>
+                        <td>09-01-2024</td>
+                        <td>Hasil Seleksi Calon Pegawai Negeri Sipil (CPNS) Kementerian Agama Republik Indonesia Tahun Anggaran 2023</td>
+                        <td><a href="https://kemenag.go.id/informasi/hasil-seleksi-calon-pegawai-negeri-sipil--cpns--kementerian-agama-republik-indonesia-tahun-anggaran-2023" target="_blank">Download</a></td>
+                      </tr>
+                      <tr>
+                        <td>18-01-2024</td>
+                        <td>Pengumuman Hasil Seleksi Pasca Sanggah CPNS Kemenag 2023</td>
+                        <td><a href="https://kemenag.go.id/informasi/pengumuman-hasil-seleksi-pasca-sanggah-cpns-kemenag-2023" target="_blank">Download</a></td>
+                      </tr>
                     </tbody>
                   </table>
                 </div>
