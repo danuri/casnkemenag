47edd3939a7caec6505bd2111a0c026548387d03
danuri <danualbantani@gmail.com>
2024-09-25 22:34:12 +0700

    Update pengumuman.php

--- app/Views/pengumuman.php
+++ app/Views/pengumuman.php
@@ -32,6 +32,11 @@
                         <td>Pengumuman CPNS Kemenag 2024</td>
                         <td><a href="https://kemenag.go.id/informasi/pengumuman-cpns-kemenag-2024" target="_blank">Download</a></td>
                       </tr>
+<tr>
+                        <td>25-09-2024</td>
+                        <td>Informasi Daftar Pelamar Perubahan Status Kelulusan Seleksi Administrasi CPNS Tahun Anggaran 2024</td>
+                        <td><a href="https://ropeg.kemenag.go.id:9000/cdn/Informasi-Daftar-Pelamar-Perubahan-Status-Kelulusan-Seleksi-Administrasi-CPNS-Tahun-Anggaran-2024.pdf" target="_blank">Download</a></td>
+                      </tr>
                     </tbody>
                   </table>
                 </div>
