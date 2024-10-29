abe850ed14d739fa326402cd07c3b769caad24b5
danuri <danualbantani@gmail.com>
2024-10-29 20:50:21 +0700

    home

--- app/Views/index.php
+++ app/Views/index.php
@@ -12,73 +12,99 @@
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-xxl-7">
-        <!-- <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-post="casnkemenag/103" data-width="100%"></script> -->
-
+    <div class="row justify-content-center">
+      <div class="col-lg-10">
         <div class="card">
-          <div class="card-header align-items-center d-flex">
-            <h4 class="card-title mb-0 flex-grow-1">Jadwal Pendaftaran CASN 2024</h4>
-          </div>
-          <div class="card-body">
-            <div class="table-responsive table-card">
-              <table class="table table-borderless table-hover table-nowrap align-middle mb-0">
-                <thead class="table-light">
-                  <tr>
-                    <th>Jenis Pengadaan</th>
-                    <th>Mulai</th>
-                    <th>Akhir</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>CPNS</td>
-                    <td>01 September 2024</td>
-                    <td>14 September 2024</td>
-                  </tr>
-                  <tr>
-                    <td>CPPPK</td>
-                    <td>Belum Tersedia</td>
-                    <td>Belum Tersedia</td>
-                  </tr>
-                </tbody>
-              </table>
+          <div class="bg-warning-subtle position-relative">
+            <div class="card-body p-5">
+              <div class="text-center">
+                <h3>Tata Cara Pendaftaran PPPK<br>Kementerian Agama</h3>
+                <p class="mb-0 text-muted">Tahun Anggaran 2024</p>
+              </div>
+            </div>
+            <div class="shape">
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="1440" height="60" preserveAspectRatio="none" viewBox="0 0 1440 60">
+                <g mask="url(&quot;#SvgjsMask1001&quot;)" fill="none">
+                  <path d="M 0,4 C 144,13 432,48 720,49 C 1008,50 1296,17 1440,9L1440 60L0 60z" style="fill: var(--vz-secondary-bg);"></path>
+                </g>
+                <defs>
+                  <mask id="SvgjsMask1001">
+                    <rect width="1440" height="60" fill="#ffffff"></rect>
+                  </mask>
+                </defs>
+              </svg>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="col-xxl-5">
-        <div class="d-flex flex-column h-100">
-          <div class="row h-100">
-            <div class="col-12">
-
-              <div class="card">
-                <div class="card-body p-0">
-                  <div class="alert alert-warning border-0 rounded-0 m-0 d-flex align-items-center" role="alert">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-triangle text-warning me-2 icon-sm"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
-                    <div class="flex-grow-1 text-truncate">
-                      Perhatian
-                    </div>
-                  </div>
-
-                  <div class="row align-items-end">
-                    <div class="col-sm-8">
-                      <div class="p-3">
-                        <p class="fs-16 lh-base">Pendaftaran hanya melalui portal SSCASN BKN <i class="mdi mdi-arrow-right"></i></p>
-                        <div class="mt-3">
-                          <a href="https://sscasn.bkn.go.id/" class="btn btn-success" target="_blank">Lihat Portal SSCASN!</a>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-sm-4">
-                      <div class="px-3">
-                        <img src="assets/images/user-illustarator-2.png" class="img-fluid" alt="">
-                      </div>
-                    </div>
-                  </div>
-                </div> <!-- end card-body-->
+          <div class="card-body p-4">
+            <div class="d-flex">
+              <div class="flex-shrink-0 me-3">
+                <i data-feather="check-circle" class="text-success icon-dual-success icon-xs"></i>
               </div>
-            </div> <!-- end col-->
+              <div class="flex-grow-1">
+                <h5>Cek Formasi Pada Aplikasi PDM</h5>
+                <p class="text-muted">Anda diwajibkan mengakses laman PDM Non ASN melalui aplikasi <a href="https://pdm-nonasn.kemenag.go.id/" target="_blank">https://pdm-nonasn.kemenag.go.id/</a>.</p>
+                <p class="text-muted">Formasi yang Anda lamar, <strong>harus sesuai</strong> dengan Formasi yang tertera pada aplikasi PDM.</p>
+                <p class="text-muted">Ketidaksesuaian formasi yang dilamar pada SSCASN akan mengakibatkan status kelulusan Administrasi Anda.</p>
+                <p class="text-muted">Berikut Tata Cara cek Formasi pada PDM:</p>
+                <ul class="text-muted">
+                  <li>
+                    <p>Buka Aplikasi PDM Non-ASN melalui <a href="https://pdm-nonasn.kemenag.go.id/" target="_blank">https://pdm-nonasn.kemenag.go.id/</a>.</p>
+                  </li>
+                  <li>
+                    <p>Login menggunakan NIK dan Password yang digunakan sebelumnya.</p>
+                  </li>
+                  <li>
+                    <p>Perhatikan Informasi yang tertera pada Aplikasi PDM Non-ASN.</p>
+                  </li>
+                  <li>
+                    <p>Klik <strong>Generate</strong>.</p>
+                  </li>
+                  <li>
+                    <p>Unduh/<i>Download</i> Bukti cek formasi yang sudah disediakan.</p>
+                  </li>
+                  <li>
+                    <p>Dokumen yang diunduh menjadi persyaratan Anda ketika melamar PPPK pada SSCASN.</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="d-flex">
+              <div class="flex-shrink-0 me-3">
+                <i data-feather="check-circle" class="text-success icon-dual-success icon-xs"></i>
+              </div>
+              <div class="flex-grow-1">
+                <h5>Daftar Akun SSCASN</h5>
+                <p class="text-muted">Pendaftaran PPPK dilakukan melalui https://sscasn.bkn.go.id/. Anda diharuskan membuat akun terlebih dahulu. Pastikan pembuatan Akun menggunakan NIK yang sesuai dengan yang terdaftar pada Aplikasi Non-ASN</p>
+                <p class="text-muted">Petunjuk pendaftaran Akun SSCASN:</p>
+                <ul class="text-muted vstack gap-2">
+                  <li>
+                    Silahkan baca melalui <a href="https://sscasn.bkn.go.id/bukupetunjuk" target="_blank">https://sscasn.bkn.go.id/bukupetunjuk</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="d-flex">
+              <div class="flex-shrink-0 me-3">
+                <i data-feather="check-circle" class="text-success icon-dual-success icon-xs"></i>
+              </div>
+              <div class="flex-grow-1">
+                <h5>Daftar Formasi PPPK Kementerian Agama</h5>
+                <p class="text-muted">Sebelum Anda mendaftar, baca kembali pengumuman terkait dokumen persyaratan pada <a href="https://kemenag.go.id/informasi/pengumuman-pengadaan-pppk-kemenag-formasi-2024" target="_blank">Pengumuman Pengadaan PPPK Kemenag Formasi 2024</a>.</p>
+                <p class="text-muted">Melamar Formasi:</p>
+                <ul class="text-muted vstack gap-2">
+                  <li>
+                    Silahkan baca melalui <a href="https://sscasn.bkn.go.id/bukupetunjuk" target="_blank">https://sscasn.bkn.go.id/bukupetunjuk</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+
+            <div class="text-end">
+              <a href="#!" class="btn btn-danger">I'm Understand</a>
+            </div>
           </div>
         </div>
       </div>
